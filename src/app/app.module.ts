f40bf9d0140ff943ca0935097c260cdbd983af7f
Danil Katuhin <the.darkvood@mail.ru>
2019-11-02 01:58:40 +0300

    Верстка главной страницы

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LessonFiltersComponent } from './lesson-filters/lesson-filters.component';
+import { LessonComponent } from './lesson/lesson.component';
+import { LessonListComponent } from './lesson-list/lesson-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LessonFiltersComponent,
+    LessonComponent,
+    LessonListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/lesson-filters/lesson-filters.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lesson-filters',
+  templateUrl: './lesson-filters.component.html',
+  styleUrls: ['./lesson-filters.component.scss']
+})
+export class LessonFiltersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
