b5138eb9dbe5054d9f571798d865edd259c5b873
Danil Katuhin <the.darkvood@mail.ru>
2019-11-02 18:26:53 +0300

    Добавление роутинга и группировка компонентов

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LessonFiltersComponent } from './lesson-filters/lesson-filters.component';
-import { LessonComponent } from './lesson/lesson.component';
-import { LessonListComponent } from './lesson-list/lesson-list.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LessonFiltersComponent} from './main-page/lesson-filters/lesson-filters.component';
+import {LessonComponent} from './main-page/lesson/lesson.component';
+import {LessonListComponent} from './main-page/lesson-list/lesson-list.component';
+import {MainPageComponent} from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LessonFiltersComponent,
     LessonComponent,
-    LessonListComponent
+    LessonListComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
@@ -21,4 +23,5 @@ import { LessonListComponent } from './lesson-list/lesson-list.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
