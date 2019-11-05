406708981685142dc8cf8a1baa64089678631c63
Danil Katuhin <the.darkvood@mail.ru>
2019-11-05 23:32:59 +0300

    Добавлена страница "О проекте"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {MainPageComponent} from './main-page/main-page.component';
 import {LessonPageComponent} from './lesson-page/lesson-page.component';
+import {AboutPageComponent} from './about-page/about-page.component';
 
 const routes: Routes = [
   {path: '', component: MainPageComponent},
-  {path: 'lesson/:uid', component: LessonPageComponent}
+  {path: 'lesson/:uid', component: LessonPageComponent},
+  {path: 'about', component: AboutPageComponent}
 ];
 
 @NgModule({
