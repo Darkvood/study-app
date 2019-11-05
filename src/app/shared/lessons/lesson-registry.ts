7a6191fcc1fb6e839a9a78482dbafb3150847991
Danil Katuhin <the.darkvood@mail.ru>
2019-11-06 00:28:05 +0300

    Добавлены уроки: "Лососевые рыбы" и "Осетровые рыбы"

--- src/app/shared/lessons/lesson-registry.ts
+++ src/app/shared/lessons/lesson-registry.ts
@@ -1,3 +1,5 @@
 export {СzechIntroduction} from './items/сzech-introduction';
 export {СzechPronouns} from './items/сzech-pronouns';
+export {FishSturgeon} from './items/fish-sturgeon';
+export {FishSalmonidae} from './items/fish-salmonidae';
 export {FruitLesson} from './items/fruit-lesson';
