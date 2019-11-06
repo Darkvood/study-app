3b592e3f3732dff3a0e337fe786997f0810f3864
Danil Katuhin <the.darkvood@mail.ru>
2019-11-07 01:28:32 +0300

    Добавлен урок "Карповые рыбы" и отформатированы старые

--- src/app/shared/lessons/lesson-registry.ts
+++ src/app/shared/lessons/lesson-registry.ts
@@ -3,3 +3,4 @@ export {СzechPronouns} from './items/сzech-pronouns';
 export {СzechByt} from './items/сzech-byt';
 export {FishSturgeon} from './items/fish-sturgeon';
 export {FishSalmonidae} from './items/fish-salmonidae';
+export {FishCyprinidae} from './items/fish-cyprinidae';
