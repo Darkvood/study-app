3062a4198abd800a01bf0b620c8e6228d7dc37ac
Danil Katuhin <the.darkvood@mail.ru>
2019-11-06 22:34:20 +0300

    Теоритическая часть урока "Чешский язык. Глагол «Быть» (být)"

--- src/app/shared/lessons/lesson-registry.ts
+++ src/app/shared/lessons/lesson-registry.ts
@@ -1,4 +1,5 @@
 export {СzechIntroduction} from './items/сzech-introduction';
 export {СzechPronouns} from './items/сzech-pronouns';
+export {СzechByt} from './items/сzech-byt';
 export {FishSturgeon} from './items/fish-sturgeon';
 export {FishSalmonidae} from './items/fish-salmonidae';
